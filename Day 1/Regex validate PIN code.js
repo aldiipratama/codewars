5a9970e87809d8b5aa2cdf896af6fbda7faa3ae9
aldiipratama <paldi0013@gmail.com>
2024-01-29 16:40:27 +0700

    CODEWARS CHALLANGE DAY 1

--- Day 1/Regex validate PIN code.js
+++ Day 1/Regex validate PIN code.js
@@ -1,3 +1,14 @@
+/**
+ * DESCRIPTION:
+ * ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+ * If the function is passed a valid PIN string, return true, else return false.
+ *
+ * Examples (Input --> Output)
+ * - "1234"   -->  true
+ * - "12345"  -->  false
+ * - "a234"   -->  false
+ */
+
 const validatePIN = (pin) =>
   pin.match(/^(\d{4}$)/) || pin.match(/^(\d{6}$)/) ? true : false;
 
--- Day 1/Vowel Count.js
+++ Day 1/Vowel Count.js
@@ -1,3 +1,10 @@
+/**
+ * DESCRIPTION:
+ * Return the number (count) of vowels in the given string.
+ * We will consider a, e, i, o, u as vowels for this Kata (but not y).
+ * The input string will only consist of lower case letters and/or spaces.
+ */
+
 function getCount(str) {
   const vowelCount = str.match(/[aiueo]/gi);
   return !vowelCount ? 0 : vowelCount.length;
